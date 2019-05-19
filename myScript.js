d0ecf20b3865ea83e8a27ff8b603dcc3803145c7
jhon-swai <jhonswai@gmail.com>
2019-05-19 09:45:49 +0300

    functional navigation bar and panels

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function hideNavigationBarFunction(){
+  var smallNav =document.getElementById("smallNav");
+  if(smallNav.className.indexOf("w3-show")== -1){
+    smallNav.className += " w3-show";
+  }
+  else{
+    smallNav.className = smallNav.className.replace(" w3-show","")
+  }
+}
+function loginModal(){
+  document.getElementById()
+}

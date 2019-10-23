ccf05c926010e7d936fc5ad01552af7853b4cfb2
gsonly <gsonly.imo@gmail.com>
2019-10-23 20:33:12 +0300

    clean hideNavigationBarFunction w/ new syntax

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,19 @@
-function hideNavigationBarFunction(){
-  var smallNav =document.getElementById("smallNav");
-  if(smallNav.className.indexOf("w3-show")== -1){
-    smallNav.className += " w3-show";
+function toggleNavigationBar() {
+  let smallNav = document.getElementById("smallNav");
+
+  // check if smallNav has class with name "w3-show"
+  if (smallNav.classList.contains("w3-show")) {
+    // remove "w3-show" if already there
+    smallNav.classList.remove("w3-show");
   }
-  else{
-    smallNav.className = smallNav.className.replace(" w3-show","")
+  else {
+    // add "w3-show" if not there
+    smallNav.classList.add("w3-show");
   }
+
 }
-function loginModal(){
-  document.getElementById()
-}
+
+// commented out unnecessary function 
+// function loginModal() {
+//   document.getElementById()
+// }
